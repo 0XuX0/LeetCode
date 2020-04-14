118b285821e9e2c50dc8b24fbef2b1883c0efe87
xuchh <xuchh@fenqi.im>
2020-04-14 19:05:15 +0800

    permute solution 2

--- 46.Permute/src/Solution.java
+++ 46.Permute/src/Solution.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,15 +10,17 @@ import java.util.List;
  **/
 public class Solution {
 
-    private static List<List<Integer>> res = new ArrayList<>();
+    private static List<List<Integer>> res;
     private static boolean[] isVisited;
 
     public static List<List<Integer>> permute(int[] nums) {
+        res = new ArrayList<>();
         if (nums == null || nums.length == 0) {
             return null;
         }
         isVisited = new boolean[nums.length];
-        backTrack(nums, new ArrayList<>());
+//        backTrack(nums, new ArrayList<>());
+        backTrack2(nums,0);
         return  res;
     }
 
@@ -41,15 +44,30 @@ public class Solution {
 
     private static void backTrack2(int[] nums, int curPos) {
         if (curPos == nums.length - 1) {
-            // res add new arrayList nums
+            List<Integer> tmpResList = new ArrayList<>();
+            for (int num : nums) {
+                tmpResList.add(num);
+            }
+            res.add(tmpResList);
             return;
         }
         for(int index = curPos; index < nums.length; index++) {
             // swap(nums index curPos)
+            swap(nums, index, curPos);
             // backTrack2(nums,curPos + 1)
+            backTrack2(nums, curPos + 1);
             // swap(nums index curPos)
+            swap(nums, index, curPos);
+
         }
     }
+
+    private static void swap(int[] nums, int startPos, int endPos) {
+        int temp = nums[startPos];
+        nums[startPos] = nums[endPos];
+        nums[endPos] = temp;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1,2,3};
         List<List<Integer>> res = permute(nums);
