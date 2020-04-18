091bd448bf673de782cb5a3ceac7b93d1aae48d1
xuchh <xuchh@fenqi.im>
2020-04-18 22:17:15 +0800

    permute 2

--- 47.permuteUnique/src/Solution.java
+++ 47.permuteUnique/src/Solution.java
@@ -6,22 +6,19 @@ import java.util.List;
  * @ClassName Solution
  * @Description TODO
  * @Author 0XuX0
- * @Date 2020/4/8
+ * @Date 2020/4/18
  **/
 public class Solution {
 
-    private static List<List<Integer>> res;
+    public static List<List<Integer>> res;
     private static boolean[] isVisited;
 
-    public static List<List<Integer>> permute(int[] nums) {
+    public static List<List<Integer>> permuteUnique(int[] nums) {
         res = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
-            return null;
-        }
+        Arrays.sort(nums);
         isVisited = new boolean[nums.length];
-//        backTrack(nums, new ArrayList<>());
-        backTrack2(nums,0);
-        return  res;
+        backTrack(nums, new ArrayList<>());
+        return res;
     }
 
     private static void backTrack(int[] nums, List<Integer> tempList) {
@@ -29,8 +26,14 @@ public class Solution {
             res.add(new ArrayList<>(tempList));
             return;
         }
+
         for(int index = 0; index < nums.length; index++) {
             if (!isVisited[index]) {
+
+                if (index > 0 && !isVisited[index - 1] && nums[index] == nums[index - 1]) {
+                    continue;
+                }
+
                 isVisited[index] = true;
                 tempList.add(nums[index]);
 
@@ -40,34 +43,12 @@ public class Solution {
                 tempList.remove(tempList.size() - 1);
             }
         }
-    }
 
-    private static void backTrack2(int[] nums, int curPos) {
-        if (curPos == nums.length - 1) {
-            List<Integer> tmpResList = new ArrayList<>();
-            for (int num : nums) {
-                tmpResList.add(num);
-            }
-            res.add(tmpResList);
-            return;
-        }
-        for(int index = curPos; index < nums.length; index++) {
-            swap(nums, index, curPos);
-            backTrack2(nums, curPos + 1);
-            swap(nums, index, curPos);
-
-        }
-    }
-
-    private static void swap(int[] nums, int startPos, int endPos) {
-        int temp = nums[startPos];
-        nums[startPos] = nums[endPos];
-        nums[endPos] = temp;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3};
-        List<List<Integer>> res = permute(nums);
+        int[] nums = {1,1,2};
+        List<List<Integer>> res = permuteUnique(nums);
         res.forEach(item -> {
             for(int index = 0; index < item.size(); index ++) {
                 System.out.print(item.get(index) + " ");
