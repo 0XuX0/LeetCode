5072eec288b14b694ac13efa3a0add856aeae54e
chenghao.xu <chenghao.xu@yitu-inc.com>
2021-05-11 21:09:43 +0800

    max profit 1

---
+++ 121.MaxProfit/src/Solution.java
@@ -0,0 +1,39 @@
+/**
+ * @ClassName TODO
+ * @Description TODO
+ * @Author chenghao.xu
+ * @Date 2021/5/11
+ */
+public class Solution {
+
+    public static int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            if (i == 0) {
+                dp[0][0] = 0;
+                dp[0][1] = -prices[i];
+                continue;
+            }
+            dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            dp[i][1] = Math.max(dp[i-1][1], - prices[i]);
+        }
+        return dp[n - 1][0];
+    }
+
+    public static int maxProfit2(int[] prices) {
+        int n = prices.length;
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++) {
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, -prices[i]);
+        }
+        return dp_i_0;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = new int[]{7,1,5,3,6,4};
+        System.out.println(maxProfit(prices));
+        System.out.println(maxProfit2(prices));
+    }
+}
